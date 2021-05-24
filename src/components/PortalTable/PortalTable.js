8fa71f6e6b2f40c65cd456c6e795242004f09e94
olgapa1980 <79247892+olgapa1980@users.noreply.github.com>
2021-05-24 14:19:03 +0300

    add PortalTable.js +PortalTable.css

---
+++ src/components/PortalTable/PortalTable.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PortalTable.css'
+import server from '../../shared/server'
+import ActiveUserContext from '../../shared/activeUserContext'
+
+const PortalTable = (props) => {
+    const activeUser = useContext(ActiveUserContext);
+
+    return (
+        <div className="c-table">
+          
+        </div>
+    );
+}
+
+export default PortalTable;
