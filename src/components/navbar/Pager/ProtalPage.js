2f132da615ea1e677400d3c238af30140dad4a02
Sivan-H-A <sivananaf@gmail.com>
2021-05-24 14:55:06 +0300

    create pager component

---
+++ src/components/navbar/Pager/ProtalPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PortalPager.css';
+
+const PortalPager = (props) => {
+    const { curr } = props;
+    const { prev } = props;
+    const { next } = props;
+    
+    return (
+
+        <Container className="c-pager">
+        </Container>
+    );
+}
+
+export default PortalPager;
