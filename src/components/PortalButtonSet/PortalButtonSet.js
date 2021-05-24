26cc7b6c61ee7f8992d1f20ff1796c09e077ead7
elad <elad@cloudents.com>
2021-05-24 14:37:30 +0300

    PortalButtonSet logic

---
+++ src/components/PortalButtonSet/PortalButtonSet.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './PortalButtonSet.css';
+
+function PortalButtonSet({labels, activeButton = 0, onClick}) {
+
+    const [active, setActive] = useState(activeButton);
+
+    return (
+        <div className="c-buttonSet">
+            {
+                labels !== [] ?
+                    labels.map((label, index) => 
+                        <button key={index} className={(index === active) ? "active" : "inactive"} onClick={(e) => {setActive(index); onClick(e.target)}}>{label}</button>
+                    ) : ''
+            }
+        </div>
+    );
+}
+
+export default PortalButtonSet;
