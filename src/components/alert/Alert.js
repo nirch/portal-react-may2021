824f275d6b5520d7b7dfb7076a9639044dc89628
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-26 10:43:06 +0300

    Adding alert icons + conditional design according to type

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Alert.css'
+import error_icon from '../../assets/images/error_icon.png';
+import info_icon from '../../assets/images/info_icon.png';
 
-const AlertComponent = (props) => {
+const AlertComponent = ({text, type}) => {
+    type = "error"; // for debugging
+    text = "סיסמה שגויה";  // for debugging
     return (
         <div className="c-alert" >
             <span className="close-btn">&times;</span>
-            <div className="error-message">סיסמה שגויה</div>
+            <div className="msg-wrapper">
+                <img className="alert-icon" src={type === "error" ? error_icon : info_icon} alt=""/>
+                <span className={`alert-msg type-${type}`}>{text}</span>
+            </div>
         </div>
     );
 }
