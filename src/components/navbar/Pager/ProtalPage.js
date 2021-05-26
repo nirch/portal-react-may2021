ab78b61ece55b06cbd0bdd85fa899c119efb77a6
Sivan-H-A <sivananaf@gmail.com>
2021-05-26 10:14:57 +0300

    adding pager css + images

--- src/components/navbar/Pager/ProtalPage.js
+++ src/components/navbar/Pager/ProtalPage.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './PortalPager.css';
+import arrow_left from '../../../assets/images/arrow_left.png';
+import arrow_right from '../../../assets/images/arrow_right.png';
+import { Container } from 'react-bootstrap';
 
 const PortalPager = (props) => {
     const { curr } = props;
     const { prev } = props;
     const { next } = props;
-    
-    return (
 
-        <Container className="c-pager">
-        </Container>
+    return (
+        <div className="c-pager">
+            <img className={!prev ? "c-pager-right-op":""} src={arrow_right}></img>
+            <span>
+                {curr}
+            </span>
+            <img className={!next ? "c-pager-left-op":""} src={arrow_left}></img>
+        </div>       
     );
 }
 
