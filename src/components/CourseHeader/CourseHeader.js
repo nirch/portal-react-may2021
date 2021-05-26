535445bebd4498c050a06736f6da7cf7036f9e1e
elad <elad@cloudents.com>
2021-05-26 15:31:47 +0300

    Header layout

--- src/components/CourseHeader/CourseHeader.js
+++ src/components/CourseHeader/CourseHeader.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './CourseHeader.css';
+import BackIcon from '../../assets/icons/noun_back_arrow_2690272.png';
+import CopyIcon from '../../assets/icons/noun_copy_573715.png';
+import SaveIcon from '../../assets/icons/noun_save_2429243.png';
 
 function CourseHeader() {
     return (
         <div className="c-course-header">
-            <h1>Course Name</h1>
+            <div className="top">
+                <h1>Course Name</h1>  
+                <div className="icons">
+                    <img src={BackIcon}/>
+                    <img src={CopyIcon}/>
+                    <img src={SaveIcon}/>
+                </div>
+            </div>
             <h6>Full Course Name</h6>
         </div>
     );
