a63f486b3cffa3e0a472599c715e6e8cfeeff819
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-26 15:03:14 +0300

    Adding user page layout

--- src/pages/Users/UsersPage.js
+++ src/pages/Users/UsersPage.js
@@ -15,7 +15,26 @@ const UsersPage = (props) => {
     return (
         <div className="p-users">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>משתמשים</h1>
+            <div className="header">
+                <div>
+                    <h1>שם העובד</h1>
+                    <h1>שם משפחה</h1>
+                    <input type="checkbox" id="change-pwd" name="change-pwd"/>
+                    <label for="change-pwd">שינוי סיסמה</label>
+                    <p>נרשם ב: <span className="date-created">2018-05-08 15:36:22</span></p>
+                </div>
+                <div>
+                    <div className="actions-wrapper">
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="image-wrapper">
+                        <img className="user-image" src="" alt=""/>
+                        <img className="edit-image" src="" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
