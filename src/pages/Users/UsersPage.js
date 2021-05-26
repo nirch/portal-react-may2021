e3e8a1a27b2917a8f160b1f8f15877c8bdb6d954
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-26 15:50:38 +0300

    Adding images to user header + initial disegn

--- src/pages/Users/UsersPage.js
+++ src/pages/Users/UsersPage.js
@@ -3,6 +3,11 @@ import './UsersPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
+import back_arrow from '../../assets/images/back_arrow.svg';
+import copy_icon from '../../assets/images/copy_icon.svg';
+import save_icon from '../../assets/images/save_icon.svg';
+import profile_icon from '../../assets/images/profile_icon.svg';
+import edit_icon from '../../assets/images/edit_icon.svg';
 
 const UsersPage = (props) => {
     const { handleLogout } = props;
@@ -15,7 +20,7 @@ const UsersPage = (props) => {
     return (
         <div className="p-users">
             <PortalNavbar handleLogout={handleLogout}/>
-            <div className="header">
+            <div className="user-header">
                 <div>
                     <h1>שם העובד</h1>
                     <h1>שם משפחה</h1>
@@ -25,13 +30,15 @@ const UsersPage = (props) => {
                 </div>
                 <div>
                     <div className="actions-wrapper">
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
+                        <img src={back_arrow} alt=""/>
+                        <img src={copy_icon} alt=""/>
+                        <img src={save_icon} alt=""/>
                     </div>
                     <div className="image-wrapper">
-                        <img className="user-image" src="" alt=""/>
-                        <img className="edit-image" src="" alt=""/>
+                        <img className="user-image" src={profile_icon} alt=""/>
+                        <div className="edit-wrapper">
+                            <img className="edit-icon" src={edit_icon} alt=""/>
+                        </div>
                     </div>
                 </div>
             </div>
