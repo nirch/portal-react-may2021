e961cea76c2f8285af304977ba4fed2cdb254186
elad <elad@cloudents.com>
2021-05-26 14:56:07 +0300

    Create basic course header component

---
+++ src/components/CourseHeader/CourseHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CourseHeader.css';
+
+function CourseHeader() {
+    return (
+        <div className="c-course-header">
+            <h1>Course Name</h1>
+            <h6>Full Course Name</h6>
+        </div>
+    );
+}
+
+export default CourseHeader;
