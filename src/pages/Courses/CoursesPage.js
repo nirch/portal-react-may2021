af7f1208ef8bcc6bc4436db534a53c891ffeb894
olgapa1980 <79247892+olgapa1980@users.noreply.github.com>
2021-05-30 13:23:16 +0300

    add PortalSearchPager and PortalButtonSet to page

--- src/pages/Courses/CoursesPage.js
+++ src/pages/Courses/CoursesPage.js
@@ -1,21 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './CoursesPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
+import PortalSearchPager from '../../components/SearchPager/PortalSearchPage'
+import PortalButtonSet from '../../components/PortalButtonSet/PortalButtonSet'
+import PortalTable from '../../components/PortalTable/PortalTable'
+
+import server from '../../shared/server'
+
 
 const CoursesPage = (props) => {
     const { handleLogout } = props;
     const activeUser = useContext(ActiveUserContext);
-    
+    const [activeButton, setActiveButton] = useState(0);
+    const [activePage, setActivePage] = useState(0);
+    const [courses, setCourses] = useState('');
+  
     if (!activeUser) {
         return <Redirect to='/' />
     }
+    // let labels=[{key :activeButton, label : "עובדים פעילים" }, {key: inactiveButton, label:"לא פעילים"}];
+   let pages=1;
+    
+
+
+    let data={search: "", sorting: "courseid", desc: false, coursestatus: activeButton, page:activePage}
+    server(activeUser, data, "SearchCourses").then(res => {
+        if (res.data.error) {     
+            alert("error in get Courses");
+        } else {
+         // setCourses( res.data.courses);
+          pages=res.data.pages+1;
+        
+         
+        }
+    }, err => {        
+        console.error(err);
+    })
 
     return (
         <div className="p-courses">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>קורסים</h1>
+            <PortalSearchPager placeholder={"חיפוש קורס"}  onSearch={() => {}} pages={pages} currentPage={activePage} onPageChange={setActivePage}/>
+
+            <PortalButtonSet labels={["עובדים פעילים", "לא פעילים"]} activeButton={activeButton} changeActiveBtn={setActiveButton}  border ={"bottom"}/>   
         </div>
     );
 }
