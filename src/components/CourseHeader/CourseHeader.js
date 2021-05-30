4f16093424a7f3130290a0a974ef189110aa9e90
elad <elad@cloudents.com>
2021-05-30 10:47:20 +0300

    Get course data from server + small css fix

--- src/components/CourseHeader/CourseHeader.js
+++ src/components/CourseHeader/CourseHeader.js
@@ -4,18 +4,19 @@ import BackIcon from '../../assets/icons/noun_back_arrow_2690272.png';
 import CopyIcon from '../../assets/icons/noun_copy_573715.png';
 import SaveIcon from '../../assets/icons/noun_save_2429243.png';
 
-function CourseHeader() {
+function CourseHeader({name, subName}) {
+
     return (
         <div className="c-course-header">
             <div className="top">
-                <h1>Course Name</h1>  
+                <h1>{subName}</h1>  
                 <div className="icons">
                     <img src={BackIcon}/>
                     <img src={CopyIcon}/>
                     <img src={SaveIcon}/>
                 </div>
             </div>
-            <h6>Full Course Name</h6>
+            <h6>{name}</h6>
         </div>
     );
 }
--- src/pages/CourseDetails/CourseDetailsPage.js
+++ src/pages/CourseDetails/CourseDetailsPage.js
@@ -1,12 +1,31 @@
-import React, { useContext } from 'react';
-import './CourseDetailsPage.css'
+import React, { useContext, useEffect, useState } from 'react';
+import './CourseDetailsPage.css';
 import PortalNavbar from '../../components/navbar/PortalNavbar';
-import ActiveUserContext from '../../shared/activeUserContext'
-import { Redirect } from 'react-router-dom'
+import ActiveUserContext from '../../shared/activeUserContext';
+import { Redirect, useParams } from 'react-router-dom';
+import CourseHeader from '../../components/CourseHeader/CourseHeader';
+import server from '../../shared/server';
 
-const CourseDetailsPage = (props) => {
-    const { handleLogout } = props;
+const CourseDetailsPage = ({handleLogout}) => {
+
+    const [courseDetails, setCourseDetails] = useState({});
+
     const activeUser = useContext(ActiveUserContext);
+
+    const { id } = useParams();
+
+    const payload = {
+        "courseid": id,
+        "token": activeUser.token,
+        "v": "2.3"
+    };
+
+
+    useEffect(() => {
+        server(activeUser, payload, "GetCourseById").then(res => setCourseDetails(res.data));
+    }, []);
+    
+
     
     if (!activeUser) {
         return <Redirect to='/' />
@@ -15,7 +34,7 @@ const CourseDetailsPage = (props) => {
     return (
         <div className="p-course-details">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>פרטי קורס</h1>
+            <CourseHeader name={courseDetails.name} subName={courseDetails.subname}/>
         </div>
     );
 }
