0f4f2e768bfffcf56743bd3b36b80857914a7f38
elad <elad@cloudents.com>
2021-05-30 12:44:30 +0300

    Added two more server calls + small css fix

--- src/pages/CourseDetails/CourseDetailsPage.js
+++ src/pages/CourseDetails/CourseDetailsPage.js
@@ -9,20 +9,44 @@ import server from '../../shared/server';
 const CourseDetailsPage = ({handleLogout}) => {
 
     const [courseDetails, setCourseDetails] = useState({});
+    const [teachers, setTeachers] = useState({});
+    const [students, setStudents] = useState({});
 
     const activeUser = useContext(ActiveUserContext);
 
     const { id } = useParams();
 
-    const payload = {
-        "courseid": id,
-        "token": activeUser.token,
-        "v": "2.3"
-    };
-
 
     useEffect(() => {
+        let payload = {
+            "courseid": id,
+            "token": activeUser.token,
+            "v": "2.3"
+        };
+
         server(activeUser, payload, "GetCourseById").then(res => setCourseDetails(res.data));
+
+        payload = {
+            "courseid": id,
+            "page": 0,
+            "roleid": 1,
+            "search": "",
+            "token": activeUser.token,
+            "v": "2.3"
+        }
+
+        server(activeUser, payload, "GetCourseEnrollmentProfiles").then(res => setStudents(res.data));
+
+        payload = {
+            "courseid": id,
+            "page": 0,
+            "roleid": 2,
+            "search": "",
+            "token": activeUser.token,
+            "v": "2.3"
+        }
+
+        server(activeUser, payload, "GetCourseEnrollmentProfiles").then(res => setTeachers(res.data));
     }, []);
     
 
@@ -33,8 +57,22 @@ const CourseDetailsPage = ({handleLogout}) => {
 
     return (
         <div className="p-course-details">
-            <PortalNavbar handleLogout={handleLogout}/>
+            <PortalNavbar handleLogout={handleLogout}/>{console.log(students)}
             <CourseHeader name={courseDetails.name} subName={courseDetails.subname}/>
+            <h6>Students</h6>
+            <table>
+                <tr><th>First Name</th><th>Last Name</th></tr>
+                {students.enrolled.map((student, index) => <tr><td key={index}>{student.firstname}</td> <td key={index}>{student.lastname}</td></tr>)}
+            </table>
+            <h6>Sylabus</h6>
+            <ul>
+                {courseDetails.subjects.map((subject, index) => <li key={index}>{subject["subject"]}</li>)}
+            </ul>
+            <h6>Teachers</h6>
+            <ul>
+                {teachers.enrolled.map((teacher, index) => <li key={index}>{teacher.firstname} {teacher.lastname}</li>)}
+                {console.log(teachers)}
+            </ul>
         </div>
     );
 }
