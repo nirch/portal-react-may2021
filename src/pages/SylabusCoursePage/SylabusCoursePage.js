49211a78e9a3ad742fe9c4ae72ad3917b007c2bc
elad <elad@cloudents.com>
2021-05-30 19:37:32 +0300

    SylabusCoursePage code (logic + design)

---
+++ src/pages/SylabusCoursePage/SylabusCoursePage.js
@@ -0,0 +1,21 @@
+import './SylabusCoursePage.css';
+
+function SylabusCoursePage({courseName, subjects}) {
+
+    return (
+        <div className="p-sylabus">
+            <div className="header">
+                <div className="title">קורס</div>
+                <div className="data">{courseName}</div>
+            </div>
+            
+            <div className="list">
+                <ul>
+                    {subjects.map(subject => <li>{subject.subject}</li>)}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default SylabusCoursePage;
