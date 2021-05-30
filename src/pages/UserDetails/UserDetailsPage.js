355f95341eac0ff1bbdc6672090a6bff2640bc88
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-30 10:20:25 +0300

    move content to user details page

--- src/pages/UserDetails/UserDetailsPage.js
+++ src/pages/UserDetails/UserDetailsPage.js
@@ -3,6 +3,11 @@ import './UserDetailsPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
+import back_arrow from '../../assets/images/back_arrow.svg';
+import copy_icon from '../../assets/images/copy_icon.svg';
+import save_icon from '../../assets/images/save_icon.svg';
+import profile_icon from '../../assets/images/profile_icon.svg';
+import edit_icon from '../../assets/images/edit_icon.svg';
 
 const UserDetailsPage = (props) => {
     const { handleLogout } = props;
@@ -15,7 +20,31 @@ const UserDetailsPage = (props) => {
     return (
         <div className="p-user-details">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>פרטי משתמש</h1>
+            <div className="user-header">
+                <div>
+                    <h1>שם העובד</h1>
+                    <h1>שם משפחה</h1>
+                    <div className="checkbox-wrapper">
+                        <input type="checkbox" id="change-pwd" name="change-pwd"/>
+                        <label for="change-pwd">שינוי סיסמה</label>
+                    </div>
+                    <p>נרשם ב: <span className="date-created">2018-05-08 15:36:22</span></p>
+                </div>
+                <div>
+                    <div className="actions-wrapper">
+                        <img src={back_arrow} alt=""/>
+                        <img src={copy_icon} alt=""/>
+                        <img src={save_icon} alt=""/>
+                    </div>
+                    <div className="image-wrapper">
+                        <img className="user-image" src={profile_icon} alt=""/>
+                        <div className="edit-wrapper">
+                            <img className="edit-icon" src={edit_icon} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
