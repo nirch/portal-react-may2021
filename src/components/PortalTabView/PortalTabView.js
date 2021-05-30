f6f13d84c663675eb2e0230067daeb961e2dc97e
Dina Lewkowicz <dina4488@walla.co.il>
2021-05-30 13:03:12 +0300

    fix the padding and the width to be dynamic

--- src/components/PortalTabView/PortalTabView.js
+++ src/components/PortalTabView/PortalTabView.js
@@ -4,19 +4,24 @@ import './PortalTabView.css'
 export default function PortalTabView({tabs}) {
 
     const [tabIndex, setTabIndex] = useState(0);
-     
+
+    const style = {width:100/tabs.length + "%" }
+        
     const tabHeader = tabs.map((tab,index) => 
-                <a className={tabIndex === index ? "active" : null} onClick={(e) => setTabIndex(index)}>{tab.header}</a>);
+                <a className={tabIndex === index ? "active" : null} 
+                    onClick={(e) => setTabIndex(index)}
+                    style={style}>{tab.header}</a>);
     
+
     return (
-    <div className="c-portaltabview">
-        <div className="">
+    <div>
+        <div>
             <nav>
                 <div className="c-tabs">
                     {tabHeader}                   
                 </div>
             </nav>
-            <div className="c-tabs-view">           
+            <div>           
                 <div>{tabs[tabIndex].view}</div>
             </div>    
         </div>
