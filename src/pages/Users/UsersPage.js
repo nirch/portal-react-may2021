6c22a3091d0c8226df0fce44b345470522f50b68
GoraChitich <78313253+GoraChitich@users.noreply.github.com>
2021-05-31 11:13:28 +0300

    UserPage was finished

--- src/pages/Users/UsersPage.js
+++ src/pages/Users/UsersPage.js
@@ -2,59 +2,76 @@ import React, { useContext, useEffect, useState } from 'react';
 import './UsersPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
-import { Redirect, useLocation } from 'react-router-dom'
+import { Redirect, useHistory, useLocation } from 'react-router-dom'
 import PortalInput from '../../components/PortalInput/PortalInput';
 import PortalTable from '../../components/PortalTable/PortalTable';
 import PortalTabView from '../../components/PortalTabView/PortalTabView';
 import PortalSearchPager from '../../components/SearchPager/PortalSearchPage';
+import PortalButtonSet from '../../components/PortalButtonSet/PortalButtonSet';
+import server from '../../shared/server';
 
 const UsersPage = (props) => {
     const { handleLogout } = props;
-    const [currentTab, setCurrentTab] = useState(0);//employee working now (0), don't working (!)
-    const [searchText, setSearchText] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
+    const [userstatus, setUserstatus] = useState(1);//employee working now (1), don't working (0)
+    const [search, setSearch] = useState('');
+    const [page, setPage] = useState(0);//first page by default
+    const [countPages, setCountPages] = useState(0);//no pages by  default (because no data)
+    const [data, setData] = useState([]);//no pages by  default
+    const [currentUser, setCurrentUser] = useState(null);//no changed user
+    const [currentApiAddress, setCurrentApiAddress] = useState('');
+
+    let history = useHistory();//for go back
     const activeUser = useContext(ActiveUserContext);
     let location = useLocation();
+
     useEffect(() => {
-        console.log(location);
-      }, [location]);
+        if(location.pathname.includes("employee")){
+            setCurrentApiAddress('SearchStaffUnderMe');
+        }else if(location.pathname.includes("student")){
+            setCurrentApiAddress('SearchStudentsUnderMe');
+        }else if(location.pathname.includes("new")){
+            setCurrentApiAddress('SearchNewUsers');
+        }
+    }, [location]);
+    
+    useEffect(()=> {
+        if(currentApiAddress)
+        server(activeUser, {search, userstatus, page, desc: false, sorting: "userid"}, currentApiAddress).then(result => {
+            setCountPages(result.data.pages);
+            setData(result.data.users);
+        });
+    },[currentApiAddress,search,page,userstatus]);
       
-    useEffect(()=>{
-        console.log(`currentTab - ${currentTab}, searchText - ${searchText}, currentPage - ${currentPage}`)
-    },[currentTab, searchText, currentPage])
     if (!activeUser) {
         return <Redirect to='/' />
     }
+
+    //changed user - redirect to user's details
+    if(currentUser){
+        return <Redirect to={`/users/${currentUser.userid}`} />
+    }
     
     const header =[
-        {key: "fname", header: 'שם'},
-        {key: "lname", header: 'שם משפחה'},
+        {key: "firstname", header: 'שם'},
+        {key: "lastname", header: 'שם משפחה'},
         {key: "email", header: 'אימייל'}];
-    const data =[
-        {id: 1, fname: "Nir", lname:"Channes", email: "email@google.com"},
-        {id: 1, fname: "Nir1", lname:"Channes1", email: "email@google.com1"},
-        {id: 1, fname: "Nir2", lname:"Channes2", email: "email@google.com2"},
-        {id: 1, fname: "Nir3", lname:"Channes3", email: "email@google.com3"},
-        {id: 1, fname: "Nir4", lname:"Channes4", email: "email@google.com4"},
-        {id: 1, fname: "Nir5", lname:"Channes5", email: "email@google.com5"},
-        {id: 1, fname: "Nir6", lname:"Channes6", email: "email@google.com6"}];
     
     
     return (
         <div className="p-users">
-            <PortalNavbar handleLogout={handleLogout}/>
-            <h1>משתמשים</h1>
+            <PortalNavbar handleLogout={handleLogout} funcBack ={()=> history.goBack()}/>
 
             <PortalSearchPager 
                 placeholder='חיפוש עובד' 
-                onSearch={setSearchText}
-                pages={5} 
-                currentPage={2} 
-                onPageChange={setCurrentPage} />
-            <PortalTable headers={header} data={data} onClick={()=>{}} />
-            <PortalTabView tabs={[
-                {header: 'עובדים פעילים', onChange: setCurrentTab}, 
-                {header: 'לא פעילים', onChange:setCurrentTab}]} />
+                onSearch={setSearch}
+                pages={countPages} 
+                currentPage={page} 
+                onPageChange={(page) => setPage(page)} />
+            <PortalTable headers={header} data={data} onClick={setCurrentUser} />
+            <PortalButtonSet 
+                labels={['עובדים פעילים', 'לא פעילים']}
+                changeActiveBtn={(param) =>setUserstatus((param+1)%2)} 
+                border="top" />
 
         </div>
     );
