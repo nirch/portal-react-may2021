ef94c8bc885aa88eb03330e9e46dffd735f4dc69
Sivan-H-A <sivananaf@gmail.com>
2021-06-02 13:07:22 +0300

    fix issue from pull request: adding spinner, adding Promise.all(), change path name in redirect

--- src/components/UserCourseTab/UserCourseTab.js
+++ src/components/UserCourseTab/UserCourseTab.js
@@ -4,6 +4,7 @@ import ActiveUserContext from '../../shared/activeUserContext';
 import PortalSearchPager from '../SearchPager/PortalSearchPage';
 import { Redirect, useParams} from 'react-router';
 import PortalTable from '../PortalTable/PortalTable';
+import { Spinner } from 'react-bootstrap';
 import './UserCourseTab.css';
 
 function UserCourseTab() {
@@ -11,11 +12,11 @@ function UserCourseTab() {
     const [courses, setCourses] = useState([]);
     const [searchText, setSearchText] = useState("")
     const [roles, setRoles] = useState([]);
-    const [enrolledAsTeacher, setEnrolledAsTeacher] = useState([]) ;
-    const [enrolledAsStudent, setEnrolledAsStudent] = useState([]) ;  
     const [pages, setPages] = useState(0);
-    const [path, setPath] = useState("");
+    const [redirectToCourseId, setRedirectToCourseId] = useState("");
     const [currentPage, setCurrentPage] = useState(0);
+    const [spinner, setSpinner] = useState(false);
+
     const { id } = useParams();
 
     useEffect(() => {
@@ -30,6 +31,7 @@ function UserCourseTab() {
 
     useEffect(() => {       
         if(activeUser && id){
+            setSpinner(true);
             getAllCourses();   
         }
     }, [activeUser,id, searchText, currentPage, roles]);
@@ -38,29 +40,29 @@ function UserCourseTab() {
         return <Redirect to='/' />
     }
 
-    if(path){
-        return <Redirect to={path}/>
+    if(redirectToCourseId){
+        return <Redirect to={redirectToCourseId}/>
     }
 
     async function getAllCourses(){
         const data = {userId: id, search:searchText, page:currentPage}
         let tempCourseArr = [];
         let pages = 0;
+        const promises =[]
         for (let i=0; i<roles.length;i++){
             data.roleid = roles[i];
-            const coursesResult = await server(activeUser, data, "GetCoursesWithUserEnrolledAsRole");
-            //prepare the data srtucture for future use as old portal uses this.
-            const enrolled = coursesResult.data.enrolled;
-            if(roles[i]===1){
-                setEnrolledAsTeacher(enrolled);
-            }else if(roles[i]===2){
-                setEnrolledAsStudent(enrolled);
-            }
-            tempCourseArr = tempCourseArr.concat(enrolled)
-            pages = Math.max(pages, coursesResult.data.pages);
+            promises.push(server(activeUser, data, "GetCoursesWithUserEnrolledAsRole"));
         } 
-        setPages(pages);
-        setCourses(tempCourseArr);
+        const response = await Promise.all(promises);
+        if(response && response.length>0){
+            for (let i=0;i<response.length;i++){
+                pages = Math.max(pages, response[i].data.pages);
+                tempCourseArr = tempCourseArr.concat(response[i].data.enrolled);
+            }
+            setPages(pages);
+            setCourses(tempCourseArr);
+        }
+        setSpinner(false);
     }  
 
     const headers= [
@@ -73,7 +75,7 @@ function UserCourseTab() {
     }
 
     function onRowClick(data){
-        setPath(`/courses/${data.courseid}`);      
+        setRedirectToCourseId(`/courses/${data.courseid}`);      
     }
 
     function onSearch(searchText){
@@ -84,19 +86,22 @@ function UserCourseTab() {
 
     return (
         <>
-            {courses && courses.length>0 ?
-                <div className="c-user-course-tab">
-                    <PortalSearchPager placeholder="חיפוש קורס"
-                                        onSearch={onSearch}
-                                        pages={pages}
-                                        currentPage={currentPage}
-                                        onPageChange={onPageChange}/> 
+            <div className="c-user-course-tab">
+                <PortalSearchPager placeholder="חיפוש קורס"
+                                onSearch={onSearch}
+                                pages={pages}
+                                currentPage={currentPage}
+                                onPageChange={onPageChange}/> 
+                {spinner ? 
+                    <Spinner animation="border" role="status" />
+                : null}    
+                {courses && courses.length>0 ?
                     <PortalTable headers={headers} 
                                 data={courses} 
                                 onClick={onRowClick}/>
-                </div>
-            :null
-            }
+                                : null
+                }
+            </div>
         </>
     )
 }
